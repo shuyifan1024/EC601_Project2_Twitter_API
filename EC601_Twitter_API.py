9303514a676c43c05c7a3a674787ccffe3330fbe
shuyifan1024 <90437122+shuyifan1024@users.noreply.github.com>
2021-10-09 10:58:09 -0400

    Update EC601_Twitter_API.py

--- EC601_Twitter_API.py
+++ EC601_Twitter_API.py
@@ -3,22 +3,23 @@ import json
 import sys
 
 # input the keys and secret to acquire permission of API
-consumerKey = "5cQw8mSauZEG5A4Frn2uMQrnk"
-consumerSecret = "Ig1myJTZOR5VF2HMezfVd2n33ZfhSIWbYUpuBfiBfSEuvwto1y"
-accessToken = "1441983537008152581-tPEF6LEkZIuSfEKSc9AhBVs8tj023Y"
-accessTokenSecret = "f65k2QiGQEBOvHIbVIAfU0MjTZJfLg2FMVHZcKkQqhi1M"
+consumerKey = "5cQw8mS*********uMQrnk"
+consumerSecret = "Ig1myJTZOR5VF**********ZfhSIWbYUpu*******vwto1y"
+accessToken = "14419835**********-tPE******************s8tj023Y"
+accessTokenSecret = "f65k2Qi*********bVIAfU0*************Qqhi1M"
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-KEY = input(" Input KEYWORDS that you want to find: ")
+KEY = input("Input KEYWORDS that you want to find: ")
 NUM = int(input("Input the NUMBER that you need : "))
-
 LIST = []  # creat a new list to save data
-
-search = tweepy.Cursor(api.search_tweets, KEY).items(NUM)  # use the API of search
+# use the API of search
+search = tweepy.Cursor(api.search_tweets, KEY).items(NUM)
 
 for alltweets in search:
     LIST.append(alltweets.text)
 
-print("This is what you want to find :", LIST)
+#output the data
+LIST = pd.DataFrame(LIST)
+LIST
